d54ac409f99b6798440541410d6ede5360234de5
Sorenson Joshua <jdsorenson@pm.me>
2022-12-01 17:27:35 -0800

    Enable reading a file

--- src/bin/day1/main.rs
+++ src/bin/day1/main.rs
@@ -1,3 +1,8 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+   let contents = fs::read_to_string("./src/bin/day1/input.txt")
+     .expect("Should have been able to read the file");
+   println!("{contents}");
 }
