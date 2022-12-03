932f70f57227aa2a8f5daeb3e8d5cd70d89a50e5
Sorenson Joshua <jdsorenson@pm.me>
2022-12-03 08:04:07 -0800

    Handwrite bit logic

--- src/bin/day3/main.rs
+++ src/bin/day3/main.rs
@@ -1,93 +1,66 @@
 #![feature(array_chunks)]
-use bitvec::prelude::*;
-use std::{collections::HashSet, fs};
+use std::fs;
 
-#[derive(Debug)]
-struct Rucksack {
-    compartment_1: BitArray,
-    compartment_2: BitArray,
-}
+// static CONTENTS: &str = include_str!("input.txt");
+pub fn compartments_from_str(line: &str) -> [usize; 2] {
+    let wonky_remapper = vec![
+        27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49,
+        50, 51, 52, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17,
+        18, 19, 20, 21, 22, 23, 24, 25, 26,
+    ];
 
-impl Rucksack {
-    pub fn new(compartment_1: BitArray, compartment_2: BitArray) -> Self {
-        assert_eq!(compartment_1.len(), compartment_2.len());
-        Self {
-            compartment_1,
-            compartment_2,
-        }
+    let mut compartment_1 = 0;
+    let mut compartment_2 = 0;
+
+    let (first_half, second_half) = line.split_at(line.len() / 2);
+
+    for index in first_half.chars().map(|c| wonky_remapper[c as usize - 65]) {
+        compartment_1 = compartment_1 | 1 << index;
     }
-
-    pub fn from_str(line: &str) -> Self {
-        let wonky_remapper = vec![
-            27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48,
-            49, 50, 51, 52, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26,
-        ];
-
-        let mut compartment_1 = bitarr![0; 52];
-        let mut compartment_2 = bitarr![0; 52];
-
-        let (first_half, second_half) = line.split_at(line.len() / 2);
-
-        for index in first_half.chars().map(|c| wonky_remapper[c as usize - 65]) {
-            compartment_1.set(index, true);
-        }
-        for index in second_half.chars().map(|c| wonky_remapper[c as usize - 65]) {
-            compartment_2.set(index, true);
-        }
-        Self::new(compartment_1, compartment_2)
+    for index in second_half.chars().map(|c| wonky_remapper[c as usize - 65]) {
+        compartment_2 = compartment_2 | 1 << index;
     }
-    pub fn contents(&self) -> BitVec {
-        self.compartment_1
-            .iter()
-            .by_vals()
-            .zip(self.compartment_2.iter().by_vals())
-            .map(|(x1, x2)| x1 || x2)
-            .collect::<BitVec>()
-    }
-
-    pub fn find_duplicate_item_value(&self) -> usize {
-        self.compartment_1
-            .iter()
-            .by_vals()
-            .zip(self.compartment_2.iter().by_vals())
-            .enumerate()
-            .map(|(e, (x1, x2))| if x1 && x2 { e } else { 0 })
-            .sum::<usize>()
-    }
-}
-
-fn find_group_badge_value(group: &[Rucksack; 3]) -> usize {
-    let badge_value_sum = group[0]
-        .contents()
-        .iter()
-        .by_vals()
-        .zip(group[1].contents().iter().by_vals())
-        .zip(group[2].contents().iter().by_vals())
-        .map(|((x1, x2), x3)| (x1, x2, x3))
-        .enumerate()
-        .map(|(e, (x1, x2, x3))| if x1 && x2 && x3 { e } else { 0 })
-        .sum::<usize>();
-    badge_value_sum
+    [compartment_1, compartment_2]
 }
 
 fn main() {
-    let contents = fs::read_to_string("./src/bin/day3/input.txt")
-        .expect("Should have been able to read the file");
+    for _ in 0..10000 {
+        let contents = fs::read_to_string("./src/bin/day3/input.txt")
+            .expect("Should have been able to read the file");
 
-    let (part_1, part_2) = contents
-        .split_whitespace()
-        .map(|r| Rucksack::from_str(r))
-        .collect::<Vec<Rucksack>>()
-        .array_chunks_mut::<3>()
-        .map(|v| {
-            let item_value_sum_3: usize = v.iter().map(|r| r.find_duplicate_item_value()).sum();
+        let (part_1, part_2) = contents
+            .split_whitespace()
+            .map(|r| compartments_from_str(r))
+            .collect::<Vec<_>>()
+            .array_chunks_mut::<3>()
+            .map(|v| {
+                let item_value_sum_3: usize = v
+                    .iter()
+                    .map(|r| {
+                        let mask = r[0] & r[1];
+                        for counter in 1..53 {
+                            if mask == (1 << counter) {
+                                return counter;
+                            }
+                        }
+                        panic!("The item number should have been found");
+                    })
+                    .sum();
 
-            let badge_value_sum = find_group_badge_value(&v);
-            (item_value_sum_3, badge_value_sum)
-        })
-        .reduce(|(acc_item, acc_badge), (items, badge)| (acc_item + items, acc_badge + badge))
-        .unwrap();
-    println!("Part 1: {:#?}", part_1);
-    println!("Part 2: {:#?}", part_2);
+                let badge_value_mask =
+                    (v[0][0] | v[0][1]) & (v[1][0] | v[1][1]) & (v[2][0] | v[2][1]);
+                let mut badge_value_sum = 0;
+                for counter in 1..53 {
+                    if badge_value_mask == (1 << counter) {
+                        badge_value_sum = counter;
+                        break;
+                    }
+                }
+                (item_value_sum_3, badge_value_sum)
+            })
+            .reduce(|(acc_item, acc_badge), (items, badge)| (acc_item + items, acc_badge + badge))
+            .unwrap();
+        println!("Part 1: {:#?}", part_1);
+        println!("Part 2: {:#?}", part_2);
+    }
 }
