b4733419224eaa9bab23350135055ab4fd16efbe
Sorenson Joshua <jdsorenson@pm.me>
2022-12-04 18:03:25 -0800

    Golf day 4

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -1,41 +1,46 @@
 #![feature(iter_array_chunks)]
 use regex::Regex;
+use std::cmp::Ordering;
 use std::collections::HashSet;
 
 static CONTENTS: &str = include_str!("input.txt");
 
+enum RangeOverlap {
+    None,
+    Intersection,
+    Contained,
+}
+
 fn main() {
     let rg = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
-    let part_1 = rg
+    let (part_1, part_2) = rg
         .captures_iter(CONTENTS)
         .map(|c| {
-            c.iter()
+            let v = c
+                .iter()
                 .skip(1)
-                .filter_map(|m| m.unwrap().as_str().parse::<usize>().ok())
-                .collect::<Vec<_>>()
-        })
-        .map(|v| {
+                .map(|m| m.unwrap().as_str().parse::<usize>().unwrap())
+                .collect::<Vec<_>>();
             assert_eq!(v.len(), 4);
-            let set_1 = (v[0]..=v[1]).collect::<HashSet<_>>();
-            let set_2 = (v[2]..=v[3]).collect::<HashSet<_>>();
-            set_1.is_subset(&set_2) || set_1.is_superset(&set_2)
+            // A-B, C-D
+            if v[1] < v[2] || v[3] < v[0] {
+                RangeOverlap::None // fully disjoint
+            } else {
+                match (v[0].cmp(&v[2]), v[1].cmp(&v[3])) {
+                    (Ordering::Equal, _) => RangeOverlap::Contained,
+                    (_, Ordering::Equal) => RangeOverlap::Contained,
+                    (Ordering::Greater, Ordering::Greater) => RangeOverlap::Intersection,
+                    (Ordering::Less, Ordering::Less) => RangeOverlap::Intersection,
+                    (Ordering::Less, _) => RangeOverlap::Contained,
+                    (Ordering::Greater, _) => RangeOverlap::Contained,
+                }
+            }
         })
-        .fold(0, |acc, item| if item { acc + 1 } else { acc });
+        .fold((0, 0), |(contained, intersections), item| match item {
+            RangeOverlap::None => (contained, intersections),
+            RangeOverlap::Intersection => (contained, intersections + 1),
+            RangeOverlap::Contained => (contained + 1, intersections + 1),
+        });
     println!("Part 1: {:#?}", part_1);
-    let part_2 = rg
-        .captures_iter(CONTENTS)
-        .map(|c| {
-            c.iter()
-                .skip(1)
-                .filter_map(|m| m.unwrap().as_str().parse::<usize>().ok())
-                .collect::<Vec<_>>()
-        })
-        .map(|v| {
-            assert_eq!(v.len(), 4);
-            let set_1 = (v[0]..=v[1]).collect::<HashSet<_>>();
-            let set_2 = (v[2]..=v[3]).collect::<HashSet<_>>();
-            !set_1.is_disjoint(&set_2)
-        })
-        .fold(0, |acc, item| if item { acc + 1 } else { acc });
     println!("Part 2: {:#?}", part_2);
 }
