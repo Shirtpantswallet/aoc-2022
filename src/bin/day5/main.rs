b84b956b013dc76a21009ec6df9632cd2ed38c87
Sorenson Joshua <jdsorenson@pm.me>
2022-12-05 01:04:28 -0800

    Refactor for-loop

--- src/bin/day5/main.rs
+++ src/bin/day5/main.rs
@@ -12,7 +12,7 @@ fn main() {
         .take_while(|l| l.len() != 0)
         // Skip the first character, which is either a ' ' or a '[',
         // then step by 4, which is skips the '   ' or '] [' between
-        // interesting characters
+        // container characters
         .flat_map(|l| {
             l.chars()
                 .skip(1)
@@ -26,7 +26,7 @@ fn main() {
         // Arguably, this '9' is a magic number, but my brain is too
         // tired to figure out how to factor it out
         .fold(vec![VecDeque::default(); 9], |mut v, (idx, character)| {
-            // the 1-based container stack index will be the last entry
+            // the 1-based container stack index will be the last entry.
             // Don't add it. Also ignore non-existent containers
             if character.is_ascii_alphabetic() {
                 // Input is parsed top to bottom, but stacks are
@@ -35,55 +35,49 @@ fn main() {
             }
             v
         });
-    // .iter()
-    // .map(|v| Into::into(v.clone()))
-    // .collect::<Vec<Vec<char>>>();
-    // for element in stacks.iter_mut() {
-    //     let new_element: Vec<char> = *element.into::<Vec<char>>();
-    //     new_element.reverse();
-    // }
+
+    // I don't think there's a way to mutate them both without cloning
+    let mut stacks_part_1 = stacks.clone();
 
     // Although the instructions are the same for parts 1 and 2,
     // the interpretation is different for each. At least we can
     // loop through them all at once!
-    let instructions = input
+    for [count, source, destination] in input
         .flat_map(|line| {
             // Skip "move", then take \d+, skip "from", take \d+, skip 2, and take final \d+
             line.split_whitespace()
                 .skip(1)
                 .step_by(2)
                 .map(|c| c.parse::<usize>().unwrap())
-        }) // Consume [count, source, destination] at a time
-        .array_chunks::<3>();
-
-    // I don't think there's a way to mutate them both without cloning
-    let mut stacks_part_1 = stacks.clone();
-
-    instructions.for_each(|[count, source, destination]| {
+        })
+        .array_chunks::<3>()
+    {
         // Although we're only looking at the length of stacks_part_1[source-1], because
         // both parts 1 and 2 begin with the same container arrangement and use the same
         // instructions, the actual count of containers will be the same
         let actual_count = stacks_part_1[source - 1].len().saturating_sub(count);
 
-        let mut fofi_containers = stacks_part_1[source - 1]
+        // FOFI -> First Out First In
+        let fofi_containers: VecDeque<_> = stacks_part_1[source - 1]
             .drain(actual_count..)
-            .collect::<Vec<char>>();
-        fofi_containers.reverse();
+            .rev()
+            .collect();
         stacks_part_1[destination - 1].append(&mut fofi_containers.into());
 
-        let same_order_containers = stacks[source - 1]
-            .drain(actual_count..)
-            .collect::<Vec<char>>();
+        let same_order_containers: VecDeque<_> = stacks[source - 1].drain(actual_count..).collect();
         stacks[destination - 1].append(&mut same_order_containers.into());
-    });
+    }
+
     let part_1 = stacks_part_1
         .iter_mut()
         .filter_map(|stack| stack.back())
         .collect::<String>();
+
     let part_2 = stacks
         .iter_mut()
         .filter_map(|stack| stack.back())
         .collect::<String>();
+
     println!("{:#?}", part_1);
     println!("{:#?}", part_2);
 }
