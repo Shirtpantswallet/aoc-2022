fe665b23d338d674b9b7b981bf5dcaa5c187d073
Sorenson Joshua <jdsorenson@pm.me>
2022-12-04 17:25:46 -0800

    Solve day 4 part 2

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -1,18 +1,10 @@
 #![feature(iter_array_chunks)]
+use regex::Regex;
 use std::collections::HashSet;
 
-use regex::Regex;
 static CONTENTS: &str = include_str!("input.txt");
 
-fn overlaps_from_str(line: &str) -> Option<bool> {
-    // let rg = Regex::new(r"(?P<low_1>\d+)-(?P<high_1>\d+),(?P<low_2>\d+)-(?P<high_2>\d+)");
-    // line.map();
-    todo!()
-}
 fn main() {
-    // let contents = fs::read_to_string("./src/bin/day3/input.txt")
-    //     .expect("Should have been able to read the file");
-
     let rg = Regex::new(r"(\d+)-(\d+),(\d+)-(\d+)").unwrap();
     let part_1 = rg
         .captures_iter(CONTENTS)
@@ -30,4 +22,20 @@ fn main() {
         })
         .fold(0, |acc, item| if item { acc + 1 } else { acc });
     println!("Part 1: {:#?}", part_1);
+    let part_2 = rg
+        .captures_iter(CONTENTS)
+        .map(|c| {
+            c.iter()
+                .skip(1)
+                .filter_map(|m| m.unwrap().as_str().parse::<usize>().ok())
+                .collect::<Vec<_>>()
+        })
+        .map(|v| {
+            assert_eq!(v.len(), 4);
+            let set_1 = (v[0]..=v[1]).collect::<HashSet<_>>();
+            let set_2 = (v[2]..=v[3]).collect::<HashSet<_>>();
+            !set_1.is_disjoint(&set_2)
+        })
+        .fold(0, |acc, item| if item { acc + 1 } else { acc });
+    println!("Part 2: {:#?}", part_2);
 }
