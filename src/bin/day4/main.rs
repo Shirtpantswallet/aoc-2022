35bed99f42ca56b632edbb2b5627753077087fd2
Sorenson Joshua <jdsorenson@pm.me>
2022-12-04 19:35:02 -0800

    Remove Ordering

--- src/bin/day4/main.rs
+++ src/bin/day4/main.rs
@@ -1,5 +1,4 @@
 #![feature(iter_array_chunks)]
-use std::cmp::Ordering;
 
 static CONTENTS: &str = include_str!("input.txt");
 
@@ -18,12 +17,10 @@ fn main() {
         .map(|v| {
             if v[1] < v[2] || v[0] > v[3] {
                 RangeOverlap::None
+            } else if (v[0] > v[2] && v[1] > v[3]) || (v[0] < v[2] && v[1] < v[3]) {
+                RangeOverlap::Intersection
             } else {
-                match (v[0].cmp(&v[2]), v[1].cmp(&v[3])) {
-                    (Ordering::Greater, Ordering::Greater) => RangeOverlap::Intersection,
-                    (Ordering::Less, Ordering::Less) => RangeOverlap::Intersection,
-                    (_, _) => RangeOverlap::Contained,
-                }
+                RangeOverlap::Contained
             }
         })
         .fold((0, 0), |(contained, intersections), item| match item {
