657e7eb0d3a1bb6213f8ac1c5e5b629691907120
Sorenson Joshua <jdsorenson@pm.me>
2022-12-06 05:15:17 -0800

    Reduce day 6 part 1 complexity

--- src/bin/day6/main.rs
+++ src/bin/day6/main.rs
@@ -1,28 +1,28 @@
 static CONTENTS: &str = include_str!("input.txt");
 
 fn main() {
-    let datastream = CONTENTS.as_bytes();
-    let leading_indexes = datastream
-        .windows(4)
-        .enumerate()
-        .filter_map(|(index, window)| {
-            if let [first, second, third, fourth] = window {
-                if first != second
-                    && first != third
-                    && first != fourth
-                    && second != third
-                    && second != fourth
-                    && third != fourth
-                {
-                    Some(index)
-                } else {
-                    None
-                }
+    let datastream: &[u8] = CONTENTS.as_bytes();
+
+    // Index is the trailing character of the message, as the instructions
+    // want to know how many total characters have been processed by the
+    // *completion* of the first start-of-packet marker
+    let mut index = 3;
+    while index < datastream.len() {
+        if let [first, second, third, fourth] = datastream[(index - 3)..=index] {
+            if third == fourth {
+                index += 3;
+            } else if second == third {
+                index += 2;
+            } else if first == second {
+                index += 1;
             } else {
-                None
+                if first != third && first != fourth && second != fourth {
+                    break;
+                }
+                index += 1;
             }
-        })
-        .collect::<Vec<usize>>();
-    let part_1 = leading_indexes.first().unwrap() + 4; // + 3 for the length, + 1 for 1-indexing
-    println!("{part_1}");
+        }
+    }
+    let index = index + 1; // 1-based indexes
+    println!("Part 1: {index}");
 }
