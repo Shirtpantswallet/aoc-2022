13a9abf82e224253a46aa98cdb5e3df6affa0651
Sorenson Joshua <jdsorenson@pm.me>
2022-12-06 09:47:42 -0800

    Solve day 6 part 2

--- src/bin/day6/main.rs
+++ src/bin/day6/main.rs
@@ -1,46 +1,51 @@
 static CONTENTS: &str = include_str!("input.txt");
 
+pub fn find_index(input: &str, length: usize) -> Option<usize> {
+    if length > input.len() || length == 0 {
+        return None;
+    }
+    let input = input.as_bytes();
+    let mut index = 0;
+    while index < input.len() - length {
+        if let CollisionResult::SkipTo(stride) = find_collision(&input[index..(index + length)]) {
+            index += stride + 1;
+        } else {
+            return Some(index + length);
+        }
+    }
+    None
+}
+
+pub enum CollisionResult {
+    Unique,
+    SkipTo(usize),
+}
+
+pub fn find_collision(input: &[u8]) -> CollisionResult {
+    let mut bitarray: usize = 0;
+    for pointer in 0..input.len() {
+        if (bitarray & 1 << (input[pointer] - 'a' as u8) as usize) != 0 {
+            // Now we know there is a collision, so let's backtrack to skip forward!
+            for backtracker in 0..pointer {
+                if (input[backtracker] & input[pointer]) != 0 {
+                    return CollisionResult::SkipTo(backtracker);
+                }
+            }
+        }
+        bitarray ^= 1 << (input[pointer] - 'a' as u8);
+    }
+    CollisionResult::Unique
+}
+
 fn main() {
     let packet_length = 4;
     let part_1 = find_index(CONTENTS, packet_length).unwrap();
 
-    println!("Part 1: {part_1}");
-    // let message_length = 14;
-    // let part_2 = find_index(CONTENTS, message_length).unwrap();
-    // println!("Part 2: {part_2}");
-}
+    let message_length = 14;
+    let part_2 = find_index(CONTENTS, message_length).unwrap();
 
-pub fn find_index(input: &str, length: usize) -> Option<usize> {
-    let input = input.as_bytes();
-    // Index is the trailing character of the message, as the instructions
-    // want to know how many total characters have been processed by the
-    // *completion* of the first start-of-packet marker
-    let mut index = length - 1;
-    while index < input.len() {
-        let mut inner_index = 1 + index - length;
-        let mut bitarray: usize = 0;
-        // while no collision of characters
-        'inner: while (bitarray & 1 << (input[inner_index] - 'a' as u8)) == 0 {
-            // println!("{inner_index}");
-            bitarray ^= 1 << (input[inner_index] - 'a' as u8);
-            if inner_index == index {
-                let mut counter = 0; // c accumulates the total bits set in v
-                while counter < bitarray {
-                    bitarray &= bitarray - 1; // clear the least significant bit set
-                    counter += 1;
-                }
-                if counter == length {
-                    return Some(index + 1);
-                } else {
-                    break 'inner;
-                }
-            } else {
-                inner_index += 1;
-            }
-        }
-        index += std::cmp::max(index - inner_index, 1);
-    }
-    None
+    println!("Part 1: {part_1}");
+    println!("Part 2: {part_2}");
 }
 
 #[cfg(test)]
@@ -60,10 +65,25 @@ mod test {
         let index = find_index(input, 4).unwrap();
         assert_eq!(index, 6);
     }
+
+    #[test]
+    fn test_packet_3() {
+        let input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        let index = find_index(input, 4).unwrap();
+        assert_eq!(index, 10);
+    }
+
+    #[test]
+    fn test_packet_4() {
+        let input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+        let index = find_index(input, 4).unwrap();
+        assert_eq!(index, 11);
+    }
+
     #[test]
     fn test_message_1() {
         let input = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
-        let index = find_index(input, 14).unwrap() - 9;
+        let index = find_index(input, 14).unwrap();
         assert_eq!(index, 19);
     }
 
@@ -72,8 +92,23 @@ mod test {
         let input = "bvwbjplbgvbhsrlpgdmjqwftvncz";
         let index = find_index(input, 14).unwrap();
         assert_eq!(index, 23);
-        // nppdvjthqldpwncqszvftbrmjlhg
-        // nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
-        // zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
+    }
+    #[test]
+    fn test_message_3() {
+        let input = "nppdvjthqldpwncqszvftbrmjlhg";
+        let index = find_index(input, 14).unwrap();
+        assert_eq!(index, 23);
+    }
+    #[test]
+    fn test_message_4() {
+        let input = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+        let index = find_index(input, 14).unwrap();
+        assert_eq!(index, 29);
+    }
+    #[test]
+    fn test_message_5() {
+        let input = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+        let index = find_index(input, 14).unwrap();
+        assert_eq!(index, 26);
     }
 }
